0a8e6223a76a4e820fbc31f1a0226bea3b4e9473
yeshengtao <ystttkl@163.com>
2012-12-31 13:30:10 +0800

    create
    包含post、comment和user验证

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base
+  validates  :title, :presence=>true
+  validates  :content,:length=>{:minimum=>5}
+  has_many :comments
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogDemo::Application.config.secret_token = 'c98b80f5b8ca635ae772975a69f3d5cb14e4e81e95ace904614f4facbd90fb85fdc2ac25a7beb1406e0cba35481ff20e69cee8fc11f475af15f06efd40b04d62'
