927f565814237fa2daaff062c24dbcc95363266e
yeshengtao <ystttkl@163.com>
2013-01-12 15:19:53 +0800

    tag  module
    add tag module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,15 @@
 class Post < ActiveRecord::Base
-  validates  :title, :presence=>true
-  validates  :content,:length=>{:minimum=>5}
+  validates  :title, :presence=>true , :uniqueness =>true
+  validates  :content, :presence=>true
   has_many :comments
+  
   has_many :attachments, :as => :attachmentable
+  
+  has_many :taggings
+  has_many :tags, :through => :taggings
+  
+  
+  scope :tag_with, lambda{|tag_name| joins(:tags).where("tags.name =?",tag_name)}
+  scope :latter_than, lambda{|time| joins(:tags).where("taggings.created_at >?",time)}
+  
 end
